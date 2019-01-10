8c455e8ba4cf91a322aad3b5f2aaf7417dd8f40d
VindeeR <ericklopes47@gmail.com>
2019-01-10 16:36:01 -0200

    inicio da aula de express router

--- node-express/index.js
+++ node-express/index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = '3000';
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
-app.use(express.static(__dirname+'/public'))
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req, res, next) => {
+    res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (req, res, next) +> {
+    
+})
+
+app.use(express.static(__dirname + '/public'))
 
 app.use((req, res, next) => {
     res.statusCode = 200;
