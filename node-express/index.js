ad666f0e260227a6688ba2931a4d8871591f8925
VindeeR <ericklopes47@gmail.com>
2019-01-15 17:16:44 -0200

    +1 pko da aula

--- node-express/index.js
+++ node-express/index.js
@@ -20,9 +20,41 @@ app.get('/dishes', (req, res, next) => {
     res.end('Will send all the dishes to you!');
 });
 
-app.post('/dishes', (req, res, next) +> {
-    
-})
+app.post('/dishes', (req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name +
+        ' with deatails: ' + req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not suported on /dishes');
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('Deleting all the dishes!');
+});
+
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('Will send details of the dish: '
+        + req.params.dishId + ' to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not suported on /dishes/'
+        + req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.end('Will update the dish: '
+        + req.params.dishId);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('Deleting all the dishes!');
+});
 
 app.use(express.static(__dirname + '/public'))
 
